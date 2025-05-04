725af0418efe3876c4bc943900050336b0697ce0
Karna Gogoi <karnagogoi7@gmail.com>
2025-05-04 16:48:26 +0530

    Setup DI module

---
+++ app/src/main/java/com/example/composesample/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.example.composesample.di
+
+import com.example.composesample.data.ApiService
+import com.example.composesample.data.InsuranceRepository
+import com.example.composesample.data.InsuranceRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideApiService(): ApiService {
+        return Retrofit.Builder()
+            .baseUrl("https://6812fd78129f6313e20ffd18.mockapi.io/api/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+
+    @Provides
+    fun provideInsuranceRepository(apiService: ApiService): InsuranceRepository {
+        return InsuranceRepositoryImpl(apiService)
+    }
+}
